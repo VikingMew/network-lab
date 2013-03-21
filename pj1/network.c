7abbcc419f06d780ac022e21fe6b0b46746d06a6
vikingmew <vikingmew@vikingmew-T410.(none)>
2013-03-21 22:03:40 +0800

    select first time

--- pj1/network.c
+++ pj1/network.c
@@ -3,57 +3,68 @@
 #include <sys/types.h> 
 #include "csapp.h"
 
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
-// #include <arpa/inet.h>
-#include <sys/times.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-#include <unistd.h>
-// #include "csapp.h"
+// #include <stdio.h>
+// #include <errno.h>
+// #include <sys/times.h>
+// #include <unistd.h>
 
 #define MAX_MSG_LEN 512
 #define LENPOOL 1
 
-static char  server_ip[MAX_MSG_LEN] = "127.0.0.1";
-int          server_port = 6667;
+// static char  server_ip[MAX_MSG_LEN] = "175.186.60.211";
+int server_port = 6667;
 
 int init_listen()
 {
     int sockfd;
     int csock;
-    // sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-    char recvbuf[MAX_MSG_LEN + 1];
-    sockfd = Open_listenfd(server_port);
+
     struct sockaddr_in caddr;
-    // struct sockaddr_in servaddr;
-
-    // bzero(&servaddr, sizeof(servaddr));
-    // servaddr.sin_family = AF_INET;
-    // servaddr.sin_port = htons(server_port);
-    // inet_aton(server_ip, &servaddr.sin_addr);
-
-    // Bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-
-    // Listen(sockfd, 1);
-    // char* recbuf;
     int size;
-    // printf("Server IP %d\n", servaddr.sin_addr.s_addr);
+
+    fd_set fds;
+    int ready;
+    int max_fd;
+
+    sockfd = Open_listenfd(server_port);
+
+    FD_ZERO(&fds);
+    // FD_SET(sockfd, &read_fds);
+    // max_fd = sockfd + 1;
+
     while(1)
     {
-        printf("accept?\n");
-        csock = Accept(sockfd, (struct sockaddr*) &caddr, &size);
-        printf("size:%d\n",caddr.sin_addr);
-        rio_t rp;
-        rio_readinitb(&rp,csock);
-        while(1) {
-        bzero(&recvbuf, sizeof(recvbuf));
-        Rio_readlineb(&rp,recvbuf,MAX_MSG_LEN);
-        printf("%s",recvbuf);
-        Rio_writen(csock,recvbuf,strlen(recvbuf));
+        FD_SET(sockfd, &fds);
+        max_fd = sockfd + 1;
+        ready = Select(max_fd, &fds, 0, 0, 0);
+        if(ready == 0)
+            continue;
+        if(FD_ISSET(sockfd, &fds)){
+            csock = Accept(sockfd, (struct sockaddr*) &caddr, &size);
+            if(getpeername(csock, (struct sockaddr*) &caddr, &size) == 0)
+            {
+                printf("socket:%d\n",csock);
+                printf("ip:%s\n",inet_ntoa(caddr.sin_addr));
+                // thread(csock);
+            }
         }
     }
+    return 0;
+}
+int thread(int fd)
+{
+    char recvbuf[MAX_MSG_LEN + 1];
+    rio_t rp;
+    rio_readinitb(&rp,fd);
+    int length = 1;
+    bzero(&recvbuf, sizeof(recvbuf));
+    length = Rio_readlineb(&rp,recvbuf,MAX_MSG_LEN);
+
+    printf("%s",recvbuf);
+
+    Rio_writen(fd,recvbuf,strlen(recvbuf));//echo it
+    Close(fd);
     return 0;
 }
